9754923c5cd3e7cede4b8d0f6aa543d90f912f6b
Lucas Barallobre <lucasbarallobre@gmail.com>
2023-02-06 11:54:21 -0300

    Add colors variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,16 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.tsx"],
-  theme: {},
+  theme: {
+    colors: {
+      black: "#0a0a09",
+      blue: "#122aff",
+      current: "currentColor",
+      darkGrey: "#151515",
+      grey: "#2f2e2e",
+      lightGrey: "#838383",
+      transparent: "transparent",
+      white: "f4f8fa",
+    },
+  },
 };
