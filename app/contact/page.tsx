48294de7d04ef49741b45ef444cf3c54297e551c
“Lucas <lucasbarallobre@gmail.com>
2024-08-21 20:36:54 +0200

    feat: social media

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Linkedin,Mail} from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Mail size={20} />,
+		href: "mailto:lucasbarallobre@gmail.com",
+		label: "Email",
+		handle: "hi@lucasbara.dev",
 	},
 	{
-		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
-		label: "Email",
-		handle: "dev@chronark.com",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/lucas-barallobre/",
+		label: "LinkedIn",
+		handle: "@lucasbara",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
-		label: "Github",
-		handle: "chronark",
+		href: "https://github.com/lucasbara",
+		label: "GitHub",
+		handle: "lucasbara",
 	},
 ];
 
