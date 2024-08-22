7e3cc481e7833b617b1555426743dd97d735baef
“Lucas <lucasbarallobre@gmail.com>
2024-08-22 20:24:58 +0200

    feat: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import '../global.css';
+
 import { Inter } from '@next/font/google';
 import LocalFont from '@next/font/local';
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -12,11 +15,41 @@ const calSans = LocalFont({
   variable: '--font-calsans',
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: {
+    default: 'lucasbara.dev',
+    template: '%s | lucasbara.dev',
+  },
+  description: 'Software Engineer',
+  openGraph: {
+    title: 'lucasbara.dev',
+    description: 'Software Engineer',
+    url: 'https://lucasbara.dev',
+    siteName: 'lucasbara.dev',
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    shortcut: '/favicon.icon',
+  },
+};
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(' ')}>
       <body
