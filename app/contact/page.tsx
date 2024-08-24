447a53928f55c7961e354d097dd5318c8110359d
“Lucas <lucasbarallobre@gmail.com>
2024-08-24 19:12:09 +0200

    fix: overflow

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
   return (
     <div className="relative bg-gradient-to-tl from-zinc-900/0 via-slate-950 to-zinc-900/0">
       <Navigation />
-      <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+      <div className="container flex items-center justify-center max-h-screen px-4 mx-auto">
         <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
           {socials.map((s) => (
             <Card key={s.label}>
