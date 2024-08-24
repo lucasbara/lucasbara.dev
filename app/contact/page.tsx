c08f62fd41b1290f0151fe0c108be7bb5f697b42
“Lucas <lucasbarallobre@gmail.com>
2024-08-24 19:13:10 +0200

    fix: remove black space from bottom

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -29,7 +29,7 @@ const socials = [
 
 export default function Contact() {
   return (
-    <div className="relative bg-gradient-to-tl from-zinc-900/0 via-slate-950 to-zinc-900/0">
+    <div className="h-screen relative bg-gradient-to-tl from-zinc-900/0 via-slate-950 to-zinc-900/0">
       <Navigation />
       <div className="container flex items-center justify-center max-h-screen px-4 mx-auto">
         <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
